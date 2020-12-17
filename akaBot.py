0068b4770072e465c92516e47b9b2a53ff12939e
Yariel Mercado <yariel.then@gmail.com>
2020-12-17 17:31:26 -0400

    Added Cogs

--- akaBot.py
+++ akaBot.py
@@ -1,6 +1,24 @@
 from config import token, prefix
 from discord.ext import commands
 import discord
+import asyncio
+import os
 
-class Aka(commands.Bot):
-    '''Main bot class, inherits from '''
+bot = commands.Bot(command_prefix = prefix)
+
+@bot.command()
+async def load(ctx, cogName):
+    '''Loads a cog onto the bot.'''
+    bot.load_extension(f"cogs.{cogName}") #loads the cog in /cogs/cogName.py
+    print(f"{cogName} was loaded succesfully!")
+
+@bot.command()
+async def unload(ctx, cogName):
+    bot.unload_extension(f"cogs.{cogName}")
+    print(f"{cogName} was loaded succesfully!")
+
+if __name__ == '__main__':
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f"cogs.{filename[:-3]}") #removes the .py from CogName.py
+    bot.run(token)
