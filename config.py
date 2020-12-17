109b9a291d89b65965c4c6f4945651cdb2d6845b
Yariel Mercado <yariel.then@gmail.com>
2020-12-17 17:48:09 -0400

    Remove sensitive info

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token = "Nzg4ODM5MTc5ODMyMjYyNjg2.X9pVvA._qU3npyVO9iL7WWe0nDC-SGNKOM" #renamed to "Client Secret" in Discord's dev page
+token = "" #renamed to "Client Secret" in Discord's dev page
 
-prefix = "a!" #prefix used to invoke bot commands
+prefix = "" #prefix used to invoke bot commands
