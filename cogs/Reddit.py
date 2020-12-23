3663ad826b0e755db055b6a71a502f7e5897c41b
Yariel Mercado <yariel.then@gmail.com>
2020-12-23 18:10:21 -0400

    Finished paginator

--- cogs/Reddit.py
+++ cogs/Reddit.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from discord.ext import commands
+from disputils import BotEmbedPaginator
 import config #parent directory is a module so we can import from it (https://stackoverflow.com/questions/8951255/import-script-from-a-parent-directory/8951269)
 import praw
 import discord
-import urllib.request, json
+import asyncio
 class Reddit(commands.Cog):
     '''When the bot first joins the server.'''
     def __init__(self, bot):
@@ -18,8 +19,9 @@ class Reddit(commands.Cog):
             embed = discord.Embed(description=f"You're going too fast, slow down!", colour=discord.Color.gold())
             await ctx.send(embed=embed)
 
+
     @commands.command()
-    @commands.cooldown(5, 20, commands.BucketType.user) #can use this 4 times every 20 seconds per user
+    @commands.cooldown(5, 15, commands.BucketType.user) #can use this 4 times every 20 seconds per user
     async def sub(self, ctx, sub):
         '''Gets top hot posts from a specified subreddit. Displays four embeds at a time.'''
         sub = self.reddit.subreddit(sub)
@@ -27,22 +29,41 @@ class Reddit(commands.Cog):
             embed = discord.Embed(description=f"Can't link NSFW subreddits in a non NSFW discord channel.", colour=discord.Color.gold())
             await ctx.send(embed=embed)
             raise TypeError
-        reactions = {"left": "⬅️", "right": "➡️"}
-        desc = f"🔥Currently displaying the hottest posts for /r/{sub}!"
+
+        desc = f"🔥 Currently displaying the hottest posts for /r/{sub}!"
         time = datetime.utcnow()
         color = discord.Color.dark_red()
-        embed = discord.Embed(title = f"/r/{sub}", description = desc, url = f"https://www.reddit.com/r/{sub}", timestamp = time, colour = color)
-        #subreddit fetching
-        posts = sub.hot(limit=5)  #limit is the amount of requests we make/posts we're getting
-        for post in posts:
-            post_created = datetime.fromtimestamp(int(post.created_utc)).strftime('%m-%d-%Y at %H:%M:%S')
-            embed.add_field(name = f":arrow_up: {post.score} upvotes! | Posted by /u/{post.author.name} on {post_created}", value = f"[{post.title}](https://www.reddit.com{post.permalink})", inline = False)
-        embed.set_thumbnail(url = sub.icon_img)
-        #emoji reaction events
-        message = await ctx.send(embed = embed) #to be able to use reactions on the sent message
-        await message.add_reaction(reactions["left"])
-        await message.add_reaction(reactions["right"])
+        embeds = []
+        posts = sub.hot(limit=15)  # limit is the amount of requests we make/posts we're getting, page*number is the amt we want to request
+        postList = [items for items in posts]
+        if sub.community_icon != "":
+            icon = sub.community_icon
+        else:
+            icon = sub.icon_img
 
-#ADD MESSAGES AND REACT EMOJIS TO SCROLL THROUGH SUB
+        for i in range(3):
+            embed = discord.Embed(title = f"/r/{sub}", description = desc, url = f"https://www.reddit.com/r/{sub}", timestamp = time, colour = color)
+            #subreddit fetching
+            for post in postList[:5]:
+                post_created = datetime.fromtimestamp(int(post.created_utc)).strftime('%m-%d-%Y at %H:%M:%S')
+                embed.add_field(name = f":arrow_up: {post.score} upvotes! | Posted by /u/{post.author.name} on {post_created} UTC", value = f"[{post.title}](https://www.reddit.com{post.permalink})", inline = False)
+                embed.set_footer(text=f"Requested by {ctx.message.author}. Viewing page {i+1} out of 3.", icon_url=ctx.message.author.avatar_url)
+                embed.set_thumbnail(url= icon)
+            del postList[:5]  #removes first 5 elements since these have been added to the field already
+            embeds.append(embed)
+
+        paginator = BotEmbedPaginator(ctx, embeds)
+        await paginator.run()
+
+    @commands.command()
+    @commands.cooldown(5, 15, commands.BucketType.user)  # can use this 4 times every 20 seconds per user
+    async def subrand(self, ctx, sub):
+        '''Gets a random hot post from a subreddit.'''
+        sub = self.reddit.subreddit(sub)
+        if sub.over18 and not ctx.channel.is_nsfw():
+            embed = discord.Embed(description=f"Can't link NSFW subreddits in a non NSFW discord channel.", colour=discord.Color.gold())
+            await ctx.send(embed=embed)
+            raise TypeError
+
 def setup(bot):
     bot.add_cog(Reddit(bot))
