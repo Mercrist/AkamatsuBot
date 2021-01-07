6287950f5f4ad6c3642f6d50f048a4e1e4f571db
Yariel Mercado <yariel.then@gmail.com>
2021-01-07 18:53:48 -0400

    Need invite function complete

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -6,8 +6,9 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.archive = True
+        self.archiveChannel = None
 
-    @commands.Cog.listener()  # only need one on_error commands listener for the whole thing for slowdowns
+    @commands.Cog.listener()  #only need one on_error commands listener for the whole thing for slowdowns
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
             embed = discord.Embed(description=f"You're going too fast, slow down!", colour=discord.Color.gold())
@@ -17,10 +18,26 @@ class Admin(commands.Cog):
     async def on_message_delete(self, message):
         '''Archives and sends a message whenever it's deleted'''
         if self.archive: #only archives deleted messages if it's been enabled in said server
-            embed = discord.Embed(title=f"**Deleted Post by {message.author}**", timestamp=datetime.utcnow(), colour=discord.Color.gold())
+            embed = discord.Embed(title=f"**Deleted Post by {message.author}**", timestamp=datetime.utcnow(), colour=discord.Color.blue())
             embed.add_field(name="Deleted Content:", value= message.content, inline=False)
-            channel = self.bot.get_channel(788844685196132434)
-            await channel.send(embed = embed)
+            if self.archiveChannel is None and message.guild.system_channel is not None: #channel hasnt been set by user but the system channel is
+                channel = message.guild.system_channel
+
+            elif self.archiveChannel is not None: #channel has been set by user
+                channel = self.bot.get_channel(self.archiveChannel)
+
+            else: #any other scenario
+                channel = message.channel
+                embed = discord.Embed(description=f"Set a system channel or an archive channel!", colour=discord.Color.gold())
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def setarchive(self, ctx, channel):
+        '''Sets a channel to store deleted messages'''
+        if ctx.author.guild_permissions.administrator:
+            self.archiveChannel = int(channel.strip("<>#"))
+            embed = discord.Embed(title=f"Archive channel set to #{self.bot.get_channel(self.archiveChannel)}.", timestamp=datetime.utcnow(), colour=discord.Color.gold())
+            await ctx.channel.send(embed = embed)
 
     @commands.command()
     async def togglearchive(self, ctx):
@@ -37,14 +54,14 @@ class Admin(commands.Cog):
     @commands.command()
     async def load(self, ctx, cogName):
         '''Loads a specified Cog.'''
-        is_owner = await self.bot.is_owner(ctx.author)  # prevents other users from loading or unloading the bot
+        is_owner = await self.bot.is_owner(ctx.author)  #prevents other users from loading or unloading the bot
         if is_owner:
             self.bot.load_extension(f"cogs.{cogName}")  #loads the cog in /cogs/cogName.py
             embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
             await ctx.send(embed=embed)
 
     @commands.command()
-    async def unload(self, ctx, cogName):  # by using cogs, you dont have to turn the bot offline, simply unload the cogs with the command
+    async def unload(self, ctx, cogName):  #by using cogs, you dont have to turn the bot offline, simply unload the cogs with the command
         '''Unloads a specified Cog.'''
         is_owner = await self.bot.is_owner(ctx.author)
         if is_owner:
@@ -62,5 +79,9 @@ class Admin(commands.Cog):
             embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
             await ctx.send(embed=embed)
 
+    @commands.command()
+    async def invite(self, ctx, time = None):
+        '''Returns the bots invite link.'''
+        embed = discord.Embed(description = f"", colour=discord.Color.dark_orange())
 def setup(bot):
     bot.add_cog(Admin(bot))
