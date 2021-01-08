d23e21e367e9f6f1fcea7fb1c92e0b7f3a1c64cd
Yariel Mercado <yariel.then@gmail.com>
2021-01-08 18:17:13 -0400

    Completed archiving

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
-from datetime import datetime
 import discord
+from datetime import datetime
 class Admin(commands.Cog):
     '''Commands and events to be used or triggered for administrative purposes.'''
     def __init__(self, bot):
@@ -18,25 +18,25 @@ class Admin(commands.Cog):
     async def on_message_delete(self, message):
         '''Archives and sends a message whenever it's deleted'''
         if self.archive: #only archives deleted messages if it's been enabled in said server
-            embed = discord.Embed(title=f"**Deleted Post by {message.author}**", timestamp=datetime.utcnow(), colour=discord.Color.blue())
-            embed.add_field(name="Deleted Content:", value= message.content, inline=False)
+            embed = discord.Embed(title=f"**Deleted Post**", description = f"By @{message.author} in {message.channel.mention}", timestamp=datetime.utcnow(), colour=discord.Color.blue())
+            embed.add_field(name="Deleted Content:", value=message.content, inline=False)
             if self.archiveChannel is None and message.guild.system_channel is not None: #channel hasnt been set by user but the system channel is
                 channel = message.guild.system_channel
 
             elif self.archiveChannel is not None: #channel has been set by user
                 channel = self.bot.get_channel(self.archiveChannel)
 
-            else: #any other scenario
+            elif self.archive and message.guild.system_channel is None: #any other scenario
                 channel = message.channel
                 embed = discord.Embed(description=f"Set a system channel or an archive channel!", colour=discord.Color.gold())
-        await channel.send(embed=embed)
+            await channel.send(embed=embed)
 
     @commands.command()
     async def setarchive(self, ctx, channel):
         '''Sets a channel to store deleted messages'''
         if ctx.author.guild_permissions.administrator:
             self.archiveChannel = int(channel.strip("<>#"))
-            embed = discord.Embed(title=f"Archive channel set to #{self.bot.get_channel(self.archiveChannel)}.", timestamp=datetime.utcnow(), colour=discord.Color.gold())
+            embed = discord.Embed(description=f"Archive channel set to {self.bot.get_channel(self.archiveChannel).mention}.", timestamp=datetime.utcnow(), colour=discord.Color.gold())
             await ctx.channel.send(embed = embed)
 
     @commands.command()
@@ -49,39 +49,45 @@ class Admin(commands.Cog):
             else:
                 embed = discord.Embed(description=f"Deleted messages archiving has been disabled.", timestamp=datetime.utcnow(), colour=discord.Color.gold())
                 self.archive = False
+            await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, cogName):
+        '''Loads a specified Cog.'''
+        self.bot.load_extension(f"cogs.{cogName}")  #loads the cog in /cogs/cogName.py
+        embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def load(self, ctx, cogName):
-        '''Loads a specified Cog.'''
-        is_owner = await self.bot.is_owner(ctx.author)  #prevents other users from loading or unloading the bot
-        if is_owner:
-            self.bot.load_extension(f"cogs.{cogName}")  #loads the cog in /cogs/cogName.py
-            embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
-            await ctx.send(embed=embed)
-
-    @commands.command()
+    @commands.is_owner()
     async def unload(self, ctx, cogName):  #by using cogs, you dont have to turn the bot offline, simply unload the cogs with the command
         '''Unloads a specified Cog.'''
-        is_owner = await self.bot.is_owner(ctx.author)
-        if is_owner:
-            self.bot.unload_extension(f"cogs.{cogName}")
-            embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
-            await ctx.send(embed=embed)
+        self.bot.unload_extension(f"cogs.{cogName}")
+        embed = discord.Embed(description=f"{cogName} was unloaded succesfully!", colour=discord.Color.dark_orange())
+        await ctx.send(embed=embed)
 
     @commands.command()
+    @commands.is_owner()
     async def reload(self, ctx, cogName):
         '''Reloads the bot automatically.'''
-        is_owner = await self.bot.is_owner(ctx.author)
-        if is_owner:
-            self.bot.unload_extension(f"cogs.{cogName}")
-            self.bot.load_extension(f"cogs.{cogName}")
-            embed = discord.Embed(description=f"{cogName} was loaded succesfully!", colour=discord.Color.dark_orange())
-            await ctx.send(embed=embed)
+        self.bot.unload_extension(f"cogs.{cogName}")
+        self.bot.load_extension(f"cogs.{cogName}")
+        embed = discord.Embed(description=f"{cogName} was reloaded succesfully!", colour=discord.Color.dark_orange())
+        await ctx.send(embed=embed)
 
     @commands.command()
-    async def invite(self, ctx, time = None):
-        '''Returns the bots invite link.'''
-        embed = discord.Embed(description = f"", colour=discord.Color.dark_orange())
+    async def invite(self, ctx, time = 0):
+        '''Returns the bots invite link. Time is set in minutes'''
+        if ctx.author.guild_permissions.administrator:
+            time = time*60 #converts minutes to seconds
+            link = await ctx.channel.create_invite(max_age = time)
+            if time == 0:
+                desc = f"An invite for {ctx.guild.name} has been generated! This link has been set to never expire: {link}"
+            else:
+                desc = f"An invite for {ctx.guild.name} has been generated! Share the link for the next {time//60} minutes: {link} "
+            embed = discord.Embed(description = desc, timestamp= datetime.utcnow(), colour=discord.Color.blurple())
+            await ctx.send(embed = embed)
+
 def setup(bot):
     bot.add_cog(Admin(bot))
